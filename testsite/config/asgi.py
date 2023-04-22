1e625699f02a0ae35146d956fcf42780a8471c3d
Andrey Chiz <AChzhovjub@yandex.ru>
2023-04-22 18:12:03 +0400

    rename project from xxxsite to testsite

--- testsite/config/asgi.py
+++ testsite/config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for xxxsite project.
+ASGI config for testsite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- testsite/config/wsgi.py
+++ testsite/config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for xxxsite project.
+WSGI config for testsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
